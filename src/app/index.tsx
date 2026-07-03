774777e7af7c50501ef62155018505d2a2cec761
emmieIO <markonuoha6@gmail.com>
2026-07-03 22:44:03 +0100

    Implementednative wind + clerk auth

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,39 @@
-import { View, Text } from "react-native";
+import { useAuth } from '@clerk/expo'
+import { AuthView, UserButton } from '@clerk/expo/native'
+import { useState } from 'react'
+import { View, StyleSheet, ActivityIndicator, Button, Modal, Text } from 'react-native'
+import "./global.css";
 
-export default function Index() {
+export default function MainScreen() {
+  const { isSignedIn, isLoaded } = useAuth({ treatPendingAsSignedOut: false })
+  const [isAuthOpen, setIsAuthOpen] = useState(false)
+
+  if (!isLoaded) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
 
   return (
-    <View>
-      <Text>Welocome to Grocify</Text>
+     <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
-  );
+  )
 }
 
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
